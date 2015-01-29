be6cb3fd7d8c924b5dd0c31af7f15c24de131c8a
Joe Boland <josephjboland@gmail.com>
2015-01-29 09:18:48 -0500

    Adding backbone app, converting views to jade, convert styling to scss

---
+++ public/javascripts/app.js
@@ -0,0 +1,140 @@
+$(function(){
+
+
+  	var app = app || {};
+
+	var Word = Backbone.Model.extend({
+		defaults: {
+			index: 0,
+			simplified: '',
+			traditional: '',
+			pinyin: '',
+			definition: ''
+		},
+
+		initialize: function() {
+			console.log("creating model", this);
+
+	      	var pinyinNum = this.get("pinyin_num").match(/[0-9]/);
+	      	if (pinyinNum && pinyinNum. length > 0) {
+	      		this.set("pinyin", this.get("pinyin") + pinyinNum);
+	      	}
+
+	      // this.simplified = params['simplified'];
+	      // self.traditional = params['traditional'];
+	      // self.pinyin = params['pinyin'];
+	      // self.pinyin_accent_raw = params['pinyin_num'];
+	      // self.definition = params['definition'];
+
+	      // var accent = self.pinyin_accent_raw.match(/[0-9]/);
+
+	      // if (accent && accent.length > 0) {
+	      //   self.pinyin += accent[0];
+	      // }
+		}
+
+	});
+
+
+	var WordCollection = Backbone.Collection.extend({
+		model: Word,
+		url: '/javascripts/hanban.json'
+	});
+
+	var WordView = Backbone.View.extend({
+		el: '#flashcard-app',
+
+		// tagName: 'div',
+
+// 		template: _.template($('#flashcard-template').html()),
+
+		events: {
+			'click .prev' : 'prevWord',
+			'click .next': 'nextWord',
+			'click .header': 'toggleFlipped'
+		},
+
+		initialize: function() {
+			this.template = _.template($('#flashcard-template').html());
+			this.listenTo(this.collection, 'reset', this.pickFirst, this);
+			this.collection.fetch({reset: true});
+
+		},
+
+		render: function() {
+			// var rendered = this.collection.toJSON();
+			
+			// this.$el.html(this.template(rendered.at(0)));
+
+			console.log("rendered content", this);
+			this.collection.each(function(model, idx){
+
+				model.set("index", idx);
+				console.log('model', model.get("index"));
+			});
+			return this;
+		},
+
+		pickFirst: function() {
+			console.log("picking first item");
+			var firstWordRendered = this.collection.at(0).toJSON();
+			//console.log("firstword", firstWord);
+			//console.log("firstword", firstWord.toJSON());
+			this.$el.html(this.template(firstWordRendered));
+			
+			// set index to first word 
+			this.currentIndex = 0;
+
+			this.render();
+		},
+
+		toggleFlipped: function() {
+			console.log("in flip");
+			this.$(".flash-card-container").toggleClass("flipped");
+		},
+
+		nextWord: function() {
+			console.log('in next');
+			var nextWord;
+
+			if (this.currentIndex === this.collection.length - 1) {
+				nextWord = this.collection.findWhere({index: 0});
+
+				this.currentIndex = 0;
+			} else {
+				nextWord = this.collection.findWhere({index: this.currentIndex + 1});
+
+				this.currentIndex += 1;
+			}
+
+			this.$el.html(this.template(nextWord.toJSON()));
+		},
+
+		prevWord: function() {
+			console.log('in prev');
+			
+			var nextWord;
+
+			if (this.currentIndex === 0) {
+				nextWord = this.collection.findWhere({index: this.collection.length - 1});
+
+				this.currentIndex = this.collection.length - 1;
+			} else {
+				nextWord = this.collection.findWhere({index: this.currentIndex - 1});
+
+				this.currentIndex -= 1;
+			}
+
+			this.$el.html(this.template(nextWord.toJSON()));
+		},
+
+		currentIndex: 0
+	});
+
+	var wordList = new WordCollection();
+	var wordView = new WordView({
+		collection: wordList
+	});
+
+
+});
